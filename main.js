466d351fea17d9e8ccc0ecc5aa7caa15db2d7b30
David <davidarutyunov77@gmail.com>
2024-11-05 12:42:40 +0100

    sss

--- main.js
+++ main.js
@@ -180,19 +180,5 @@
 
 
 
-function sum() {
-  let array = [2]
 
-  return function(number) {
-    array.push(number)
-    return array.reduce((total, current) => total + current, 0)
-  }
-}
-
-
-let final = sum()
-
-
-console.log(final(5))
-console.log(final(6))
 
